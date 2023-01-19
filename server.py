b7961484426d985af9078059a5daab5017bfad37
gimonchik <73658246+fedorchikthebest@users.noreply.github.com>
2023-01-19 13:28:39 +0300

    Update server.py
    bug_fix

--- server.py
+++ server.py
@@ -4,16 +4,30 @@ import zlib
 import threading
 
 
+def edit_map(x, y):
+    with open('./data/maps/map.txt') as f:
+        mp = list(map(list, input().split('\n')))
+        if mp[y][x] == ' ':
+            mp[y][x] = '#'
+        else:
+            mp[y][x] = ' '
+
+
 def accept_connect(conn, addr):
     global players
+    p_id = ''
     while True:
-        data = loads(zlib.decompress(conn.recv(1024)))
+        data = conn.recv(1024)
+        if not data:
+            break
+        data = loads(zlib.decompress(data))
         if data.get('type') == 'exit':
             del players[data.get('p_id')]
             conn.close()
             return 0
         if data.get('type') == 'player_info':
             players[data.get('p_id')] = data.get('coords')
+            p_id = data.get('p_id')
         if data.get('type') == 'get_players':
             d = []
             for i in players.values():
@@ -26,6 +40,9 @@ def accept_connect(conn, addr):
         if data.get('type') == 'get_data':
             with open('./data/maps/map.txt') as f:
                 conn.send(zlib.compress(dumps({'map': f.read()})))
+    del players[p_id]
+    conn.close()
+    return 0
 
 
 max_players = 100
@@ -35,6 +52,7 @@ sock.listen(1)
 players = {}
 connections = []
 players_onlibe = 0
+socket.timeout = 10
 
 while True:
     if players_onlibe <= max_players:
